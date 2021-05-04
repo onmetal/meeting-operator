13342f1afbc6839bf628ee4d4e10bfbde2eee5c4
Vasilev Andrew <landeleih@protonmail.com>
2021-05-04 15:50:18 +0300

    chore: license added

--- internal/jitsi/jvb.go
+++ internal/jitsi/jvb.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package jitsi
 
 import (
@@ -17,7 +33,6 @@ const (
 	JvbPodName       = "jitsi-jvb"
 	JvbContainerName = "jvb"
 	externalPort     = 10000
-	waitForDeletion  = 40 * time.Second
 	waitForLB        = 20 * time.Second
 )
 
