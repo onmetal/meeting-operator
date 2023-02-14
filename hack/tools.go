f75382f55742f4642bf1a0fbf290c790ae793f2b
Damyan Yordanov <damyan.yordanov@sap.com>
2023-02-14 17:09:27 +0100

    Fix add license target
    Make `go.mod` consider license import, even though it's not referenced
    anywhere

--- hack/tools.go
+++ hack/tools.go
@@ -12,6 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package tools
+
+//go:build tools
+// +build tools
+
 package tools
 
 import (
